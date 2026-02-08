ca4811aacbeef4ee67ce74701e48decd7a9d77fc
Burgas <burgassme@gmail.com>
2026-02-08 21:27:48 +0700

    Adding README.md

--- src/main/kotlin/routing/Routing.kt
+++ src/main/kotlin/routing/Routing.kt
@@ -4,7 +4,7 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.csrf.*
-import io.ktor.server.plugins.statuspages.StatusPages
+import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.sessions.*
